d299d7290b3e2ee9de30c4b0814cf50ac8368c22
최은정 <qhdrn67@naver.com>
2022-01-21 10:57:42 +0900

    Styles and Placeholders

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -55,8 +55,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    background-color:${(props) => props.theme.bgColor};
-    color:${(props) => props.theme.textColor};
+    background-color:${props => props.theme.bgColor};
+    color: black;
     line-height: 1.2;
   }
   a {
