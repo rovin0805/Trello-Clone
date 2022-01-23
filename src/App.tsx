980208ccc9319a139b7be877864103d221d25b2a
Choi Eun Jeong <qhdrn67@naver.com>
2022-01-23 22:43:07 +0900

    Same Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Board from "./components/Board";
 const Wrapper = styled.div`
   display: flex;
   max-width: 680px;
-  width: 100%;
+  width: 100vw;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
@@ -15,27 +15,27 @@ const Wrapper = styled.div`
 `;
 
 const Boards = styled.div`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   width: 100%;
-  grid-template-columns: repeat(3, 1fr);
   gap: 10px;
 `;
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-    if (!destination) {
-      return;
+  const onDragEnd = (info: DropResult) => {
+    const { destination, draggableId, source } = info;
+    if (destination?.droppableId === source.droppableId) {
+      // same board movement
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination?.index, 0, draggableId);
+        return { ...allBoards, [source.droppableId]: boardCopy };
+      });
     }
-    // setToDos(prev => {
-    //   const reorderingDos = [...prev];
-    //   // 1) Delete an item on source.index
-    //   reorderingDos.splice(source.index, 1); // splice(start, length)
-    //   // 2) Put back the item on the destination.index
-    //   reorderingDos.splice(destination?.index, 0, draggableId);
-    //   return reorderingDos;
-    // });
   };
 
   return (
